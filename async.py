230aad382dc9faccc2b0aff299875219045c1e5a
Damien Carver <carverdamien@gmail.com>
2018-11-11 12:36:11 +0100

    todo: locks

--- async.py
+++ async.py
@@ -3,19 +3,25 @@ import threading
 # This function stores values from ITERABLE in a buffer.
 # If it was not able to get a new value from iterable after DELAY seconds,
 # it will flush the buffer and call ACTION on the bufferized values.
+#
+# TODO: needs lock to ensure no value losts and action is performed on values.
 def delay_action_until_iterable_blocks(iterable, action, delay=5):
     buff = []
     def func():
+        # L.LOCK()
         b = []
         while len(buff) > 0:
             b.append(buff.pop(0))
         if len(b) > 0:
             action(b)
+        # L.UNLOCK()
     t = threading.Timer(delay,func)
     t.start()
     for i in iterable:
+        # L.LOCK()
         buff.append(i)
         t.cancel()
+        # L.UNLOCK()
         t = threading.Timer(delay,func)
         t.start()
 
